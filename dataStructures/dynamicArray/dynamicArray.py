34baef6adfd6345218bb0a04563e90e2a0520788
sahiljagad <sahiljagad@gmail.com>
2024-12-20 21:24:50 -0800

    working implementation of a dynamic array in python with append, pop, insert + tests

---
+++ dataStructures/dynamicArray/dynamicArray.py
@@ -0,0 +1,72 @@
+class DynamicArray():
+
+    def __init__(self):
+        self.capacity = 5
+        self.size = 0
+        self.array = [None] * self.capacity
+    
+    def __repr__(self):
+        return self.__str__()
+  
+    def __str__(self):
+        return "[" + " ".join([str(x) for x in self.array if x != None]) + "]"
+    
+    def _grow(self):
+        self.capacity *= 2
+        self.array += [None] * (self.capacity - self.size)
+
+    def _shrink(self):
+        self.capacity //= 2
+
+    def append(self, val):
+        #check if we have reached cappacity and grow accordingly
+        if self.size == self.capacity:
+            self._grow()
+        #add value to end
+        self.array[self.size] = val
+        self.size += 1
+    
+    def pop(self, index=-1):
+        # cannot pop from empty list
+        if self.size == 0:
+            return
+        
+        # if no index is passed in, set index to last element
+        if index == -1 or index >= self.size: 
+            index = self.size - 1
+
+        # shift every element after index to left
+        for i in range(index, self.size):
+            if i == self.size - 1:
+                self.array[i] = None
+                break
+            self.array[i] = self.array[i + 1]
+
+        # decrease size by 1
+        self.size -=1
+
+        # decrease capacity if about half the elements are gone 
+        # +1 on self.size bc we can divide by 0
+        if (self.capacity // (self.size + 1)) > 1:
+            self._shrink()
+        
+    def insert(self, val, index):
+        # check if element at end or not in range of indices to be added to
+        if index >= self.size:
+            self.append(val)
+            return
+            
+        #check if we have reached cappacity and grow accordingly
+        if self.size == self.capacity:
+            self._grow()
+
+        # shift all elements from index to right
+        for i in range(self.size, index, -1):
+            self.array[i] = self.array[i - 1]
+        
+        # add value at index
+        self.array[index] = val
+
+        # increase size by 1
+        self.size +=1
+
---
+++ dataStructures/dynamicArray/test.py
@@ -0,0 +1,44 @@
+from dynamicArray import DynamicArray
+
+x = DynamicArray()
+
+# test popping empty 
+x.pop()
+assert str(x) == "[]"
+assert x.size == 0
+assert x.capacity == 5
+
+x.append(1)
+x.append(2)
+x.append(3)
+assert str(x) == "[1 2 3]"
+assert x.size == 3
+assert x.capacity == 5
+
+x.insert(0,0)
+x.insert(0, 3)
+x.insert(0, 5) 
+x.insert(4, 50)
+assert str(x) == "[0 1 2 0 3 0 4]"
+assert x.size == 7
+assert x.capacity == 10
+
+x.append(5)
+x.append(6)
+x.append(7)
+x.append(8)
+assert str(x) == "[0 1 2 0 3 0 4 5 6 7 8]"
+assert x.size == 11
+assert x.capacity == 20
+
+x.pop(0) # first 0
+x.pop(2) # second 0, index has now moved down 1 from insert at 3
+x.pop(3) # third 0 index has now moved down 2 from insert at 5
+x.pop()
+x.pop(100)
+print(x)
+assert str(x) == "[1 2 3 4 5 6]"
+assert x.size == 6
+assert x.capacity == 10
+
+print("All tests passed")
